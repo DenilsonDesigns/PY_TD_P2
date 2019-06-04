585f3e4d14264963f41eb2f624c96d7f150108aa
Languiller <Daniel.Languiller@team.telstra.com>
2019-06-05 08:56:50 +1000

    fixed game loop

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def make_teams(player_pool):
         else:
             rookie_pool.append(player_in_pool)
 
-    vets_per_team = int(experienced/3)
+    vets_per_team = int(experienced / 3)
     print(vets_per_team)
     while len(vet_pool) > 0:
         team_1.extend(vet_pool[0:vets_per_team])
@@ -46,18 +46,32 @@ def make_teams(player_pool):
     return team_1, team_2, team_3
 
 
+def get_more_stats(team_list):
+    vet_total = 0
+    rookie_total = 0
+    av_height = 0
+    guardian_list = ""
+
+    pass
+
+
 def show_menu():
     print("BASKETBALL TEAM STATS TOOL\n")
-    print("Here are your choices:\n")
-    print("1) Display Team Stats\n")
-    print("2) Quit\n\n")
+    print("-" * 20)
     show = True
     while show:
+        print("Here are your choices:\n")
+        print("1) Display Team Stats\n")
+        print("2) Quit\n\n")
         choice = input("Enter an option >")
         if choice == "2":
+            print("Exiting. Thanks for playing")
             show = False
         elif choice == "1":
+            print("Choose a team:")
             team_choice = show_teams()
+        else:
+            print("Exiting. Thanks for playing")
 
 
 def show_teams():
@@ -72,15 +86,15 @@ def print_team(team_number):
     teams_array = make_teams(player_list)
     team_list = ['Panthers', 'Bandits', 'Warriors']
     player_string = ""
-    for player_here in teams_array[team_number-1]:
-        player_string += player_here['name']+", "
-    print("Team: {} Stats\n".format(team_list[team_number-1]))
-    print("--------------------")
-    print("Total players: {}\n".format(len(teams_array[team_number-1])))
+    for player_here in teams_array[team_number - 1]:
+        player_string += player_here['name'] + ", "
+    print("Team: {} Stats\n".format(team_list[team_number - 1]))
+    print("-"*20)
+    print("Total players: {}".format(len(teams_array[team_number - 1])))
     print("Player list:")
     print(player_string)
+    print("-" * 20)
 
 
 if __name__ == '__main__':
     show_menu()
-
